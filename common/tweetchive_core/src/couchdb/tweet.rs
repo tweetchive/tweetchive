5cffdaacb229c921b7d8b28c9914e0d5df9388ce
l1npengtul <l1npengtul@protonmail.com>
2022-11-28 15:21:46 +0900

    pagination???

--- common/tweetchive_core/src/couchdb/tweet.rs
+++ common/tweetchive_core/src/couchdb/tweet.rs
@@ -1,3 +1,4 @@
+use couch_rs::CouchDocument;
 use rkyv::Archive;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -14,15 +15,17 @@ use uuid::Uuid;
     Archive,
     rkyv::Serialize,
     rkyv::Deserialize,
+    CouchDocument,
 )]
-#[cfg_attr(feature = "server", derive(couch_rs::CouchDocument))]
 pub struct Tweet {
     #[serde(skip_serializing_if = "String::is_empty")]
     pub _id: String,
+    pub also_ids: Vec<String>,
     #[serde(skip_serializing_if = "String::is_empty")]
     pub _rev: String,
     pub conversation_id: u64,
     pub quoting_id: HashMap<Uuid, u64>,
     pub reply_to: u64,
+    pub poster: u64,
     pub data: HashMap<Uuid, TweetType>,
 }
